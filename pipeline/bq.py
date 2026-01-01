b736994b3723835e92df5e477be95e18c5d7458f
kal1nadam <kalinaadam75@seznam.cz>
2026-01-01 17:34:30 +0100

    bigquery runner fix - func defined inside of init

--- pipeline/bq.py
+++ pipeline/bq.py
@@ -19,27 +19,29 @@ class BQQueryRunner:
         self.settings = settings
         self.client = bigquery.Client(project=settings.gcp_project_id, location=settings.bq_location)
 
-        def render_sql(self, sql: str) -> str:
-            """
-            Very small templating to keep SQL files portable.
-            Use placeholders like ${RAW_DATASET}, ${STG_DATASET}, ${MART_DATASET}.
-            """
-            return (
-                sql.replace("${RAW_DATASET}", self.settings.raw_dataset)
-                .replace("${STG_DATASET}", self.settings.stg_dataset)
-                .replace("${MART_DATASET}", self.settings.mart_dataset)
-                .replace("${GCP_PROJECT_ID}", self.settings.gcp_project_id)
-            )
-        
-        def run(self, sql: str, job_config: Optional[bigquery.QueryJobConfig] = None) -> QueryResult:
-            rendered = self.render_sql(sql)
-            job = self.client.query(rendered, job_config=job_config)
-            job.result() # Wait for completion
-            stats = job._properties.get("statistics", {}).get("query", {})
-            billed = int(stats.get("totalBytesBilled", 0))
-            processed = int(stats.get("totalBytesProcessed", 0))
-            return QueryResult(
-                job_id=job.job_id,
-                bytes_processed=processed,
-                bytes_billed=billed,
-            )
+    def render_sql(self, sql: str) -> str:
+        """
+        Very small templating to keep SQL files portable.
+        Use placeholders like ${RAW_DATASET}, ${STG_DATASET}, ${MART_DATASET}.
+        """
+        return (
+            sql.replace("${RAW_DATASET}", self.settings.raw_dataset)
+            .replace("${STG_DATASET}", self.settings.stg_dataset)
+            .replace("${MART_DATASET}", self.settings.mart_dataset)
+            .replace("${GCP_PROJECT_ID}", self.settings.gcp_project_id)
+        )
+    
+    def run(self, sql: str, job_config: Optional[bigquery.QueryJobConfig] = None) -> QueryResult:
+        rendered = self.render_sql(sql)
+        # print(f"Running SQL:\n{rendered}\n")
+        # return
+        job = self.client.query(rendered, job_config=job_config)
+        job.result() # Wait for completion
+        stats = job._properties.get("statistics", {}).get("query", {})
+        billed = int(stats.get("totalBytesBilled", 0))
+        processed = int(stats.get("totalBytesProcessed", 0))
+        return QueryResult(
+            job_id=job.job_id,
+            bytes_processed=processed,
+            bytes_billed=billed,
+        )
